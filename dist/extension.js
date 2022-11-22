9922561b715b2eb35500a303a2a08fb34a0c22d1
Nick Reisenauer <nickdreisenauer@gmail.com>
2022-11-21 18:02:51 -0700

    feat: setup custom command for extension

--- dist/extension.js
+++ dist/extension.js
@@ -42,8 +42,6 @@ var exports = __webpack_exports__;
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.deactivate = exports.activate = void 0;
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = __webpack_require__(1);
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -54,10 +52,8 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('vsc-hn.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from VSC HN!');
+    let disposable = vscode.commands.registerCommand("vsc-hn.startVscHn", () => {
+        vscode.window.showInformationMessage("Hello World from VSC HN!");
     });
     context.subscriptions.push(disposable);
 }
